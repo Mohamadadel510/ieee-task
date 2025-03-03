80d97cd1ac0bf806eb96cde238eae01bed2755eb
Mohamadadel510 <91435282+Mohamadadel510@users.noreply.github.com>
2025-03-03 12:08:14 +0200

    Create Task5.c

---
+++ Task5.c
@@ -0,0 +1,19 @@
+/**************************************************************************************************************************************************************************/
+//CodeForWin (functions prob.)
+// problem 1
+#include <stdio.h>
+int main (){
+ int num , cube;
+ printf("Enter a number: ");
+    scanf("%d", &num);
+    cube = num * num * num;
+    printf("Cube of %d is %d", num, cube);
+    return 0;
+}
+
+// problem 2
+
+// problem 3
+
+// problem 6 
+// problem 8
